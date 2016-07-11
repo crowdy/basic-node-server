cca5a26823f0ceb64d03e1e882abbbbdde94d61f
Dominik Kundel <dominik.kundel@gmail.com>
2016-07-11 11:01:32 +0200

    Add comments to mockdb.ts to explain features

--- api/mockdb.ts
+++ api/mockdb.ts
@@ -1,11 +1,17 @@
+// Create an interface
 export interface DataEntry {
+  // Define an optional property by appending '?' to the name
   id?: number;
 }
 
+// Declare a new type
+export type Role = 'admin' | 'user';
+
+// Extend an interface
 export interface User extends DataEntry {
   username: string;
   name: string;
-  role: 'admin' | 'user';
+  role: Role;
 }
 
 export interface Gif extends DataEntry {
@@ -13,14 +19,19 @@ export interface Gif extends DataEntry {
   url: string;
 }
 
+// Create a generic class.
+// Declare that generic type needs to extend a certain  interface
 export class MockDB<TData extends DataEntry> {
+  // Declare a private property
   private data: TData[];
   
   constructor(defaultData: TData[] = []) {
     this.data = defaultData;
   }
 
-  public get(id?: number): TData | TData[]{
+  // Define union types for functions with multiple return data
+  public get(id?: number): TData | TData[] {
+    // Do native type checking for run-time type checks
     if (typeof id === 'number') {
       return this.data[id];
     }
@@ -31,9 +42,11 @@ export class MockDB<TData extends DataEntry> {
     });
   }
 
+  // Function overloading by redefining the function
   public update(user: User): number;
   public update(id: number, user: User): number;
   public update(idOrUser: number | User, user?: User): number {
+    // Use native type checking to determine which process should be taken
     if (typeof idOrUser === 'number') {
       this.data[idOrUser] = this.clone(user);
       return idOrUser;
